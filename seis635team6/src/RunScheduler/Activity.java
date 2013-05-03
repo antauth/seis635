223d784721e001da1d0e958b108f1942e813f461
eacarrollan <eac.anderson@gmail.com>
2013-05-03 18:32:40 -0500

    Here are all the classes I created

---
+++ seis635team6/src/RunScheduler/Activity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package RunScheduler;
+
+/**
+ * @author Liz
+ *
+ */
+public class Activity {
+	
+	private float duration;
+	
+	private long date;
+	
+	private String type;
+	
+	private String title;
+	
+	private String username;
+	
+	private float distance;
+}
---
+++ seis635team6/src/test/MainTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Liz
+ *
+ */
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
