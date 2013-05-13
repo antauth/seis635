342b663a61de3a1a0dba010ef50a587bad0ff8c9
antauth <smith@antoinetteauthedna.com>
2013-05-12 21:37:17 -0500

    Completed main argument that kicks off program

--- seis635team6/src/activity_tracker/ActivityTrackerMain.java
+++ seis635team6/src/activity_tracker/ActivityTrackerMain.java
@@ -1,14 +1,23 @@
 package activity_tracker;
 
+/**
+ * 
+ * @author Antoinette
+ *
+ */
 public class ActivityTrackerMain {
 
 	/**
+	 * Required main method for Java programs.
+	 * However, all logic carried out in Controller class.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO mock login
-			//pass user identity to Controller class
+		//instantiate controller class
+		Controller controller = new Controller();
 
+		//give control to Controller for display of user prompts
+		controller.displayUserPrompt();
 	}
 
 }
